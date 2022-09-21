d05a4d46f2b091d9bf824fcfa1e9b9a2c38d853f
DELL <DELL@DESKTOP-9G2HH03>
2022-09-21 11:31:57 +0530

    TestNG Basics(Data provider,cross browser)

---
+++ TestNG/src/test/java/org/com/Simple1.java
@@ -0,0 +1,45 @@
+package org.com;
+
+import java.util.Date;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Simple1 {
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Browser launch.......");
+	}
+	@AfterClass
+	public void afterClass() {
+		System.out.println("Close Browser.......");
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		Date d =new Date(); 
+		System.out.println("Start time=" +d);
+	}
+	@AfterMethod
+	public void afterMethod() {
+		Date d =new Date(); 
+		System.out.println("End  time=" +d);	
+	}
+	@Test
+public void tc1() {
+	System.out.println("TC1......");
+	}
+	@Test
+public void tc2() {
+	System.out.println("TC2......");
+	}
+	@Test
+public void tc3() {
+	System.out.println("TC3......");
+	}
+}
+
+
+
---
+++ TestNG/src/test/java/org/com/Test3.java
@@ -0,0 +1,19 @@
+package org.com;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc1000() {
+		System.out.println("TC1000......");
+	}
+	@Test
+	public void tc2000() {
+		System.out.println("TC2000......");
+	}
+	@Test
+	public void tc3000() {
+		System.out.println("TC3000......");
+	}
+
+}
