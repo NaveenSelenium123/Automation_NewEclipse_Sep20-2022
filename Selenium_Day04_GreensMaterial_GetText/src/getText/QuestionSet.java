acdbb7f2c8646b339c31fe86a86d3e171965250a
DELL <DELL@DESKTOP-9G2HH03>
2022-10-17 10:20:23 +0530

    Selenium Day04 Gettext Method

---
+++ Selenium_Day04_GreensMaterial_GetText/src/getText/QuestionSet.java
@@ -0,0 +1,103 @@
+package getText;
+
+public interface QuestionSet {
+	/*SELENIUM
+	------------
+	DAY4
+	------------
+	Debug 
+	Xpath contains,text
+	getText();
+	getAttribute();
+
+	QUESTIONS(Theory)
+	-----------------
+	1.What is the purpose of debug?
+	2.What is the steps for debug?
+	3.What are the types of debug?
+	4.How to take a locator in which contains no tag?
+	5.How to print a paragraph from webpage?
+	6.What are the methods to get and print the text from webpage?
+	7.What is the purpose of getText() and getAttribute() ?
+	8.What is the return type of getText() and getAttribute() ?
+	9.In which interface  getText() and getAttribute() is available?
+	10.What are the difference between getText() and getAttribute()?
+	11.Whether it is possible to get the text from webpage without using getText()?
+
+	QUESTIONS(Practical)
+	--------------------
+	QUESTION 1
+	----------
+	URL : http://www.greenstechnologys.com 
+
+	NOTE: Print Greens Technologys Software Training Centers in Chennai
+
+	QUESTION 2
+	----------
+	URL : http://greenstech.in/selenium-course-content.html
+
+	NOTE: Print paragaraph starts with vel murugan.
+
+	QUESTION 3
+	----------
+	URL : http://greenstech.in/selenium-course-content.html
+
+	NOTE: Print the adayar branch address
+
+	QUESTION 4
+	----------
+	URL : http://greenstech.in/selenium-course-content.html
+
+	NOTE: Print the omr branch address
+
+	QUESTION 5
+	----------
+	URL : http://www.greenstechnologys.com/
+
+	NOTE: Print the paragraph starts with Greens Technology
+
+	QUESTION 6
+	----------
+	URL : https://www.facebook.com/ 
+
+	NOTE: Print the email and password which was entered by user 
+
+	QUESTION 7
+	----------
+	URL : http://www.google.com/
+
+	NOTE: Search greens velmurugan and click the 1st link.
+
+	QUESTION  8
+	------------
+	URL : http://adactinhotelapp.com/
+
+	NOTE: Print the UserName and Password that you are entered.
+
+	QUESTION 9
+	-----------
+	URL : https://www.snapdeal.com/ 
+
+	NOTE: Search any product and click 1st product
+
+
+	QUESTION 10
+	-----------
+	URL : https://www.flipkart.com/ 
+
+	NOTE: Search any product and click 1st product
+
+
+	QUESTION 11
+	-----------
+	URL : https://www.flipkart.com/ 
+
+	NOTE: Click login and enter User name password
+	      Print the details you are given.
+
+	QUESTION 12
+	-----------
+	URL : https://www.shopclues.com/wholesale.html 
+
+	NOTE: Search any product and click 1st product*/
+}
